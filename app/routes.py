54db540c7b8d908534e7fb17f12e00d26d7e3ae7
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-12-08 17:29:52 -0600

    render template

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
+bp = Blueprint("main", __name__, "/")
+
+
+
+@bp.route("/")
+def main():
+    return render_template('main.html')
